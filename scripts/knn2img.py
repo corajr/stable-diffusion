f4db0b23c4c2408954d9054647764e05c7c34d24
James Reynolds <magnsuviri@me.com>
2022-08-20 19:19:19 -0600

    Added mps device support

--- scripts/knn2img.py
+++ scripts/knn2img.py
@@ -309,7 +309,12 @@ if __name__ == "__main__":
     config = OmegaConf.load(f"{opt.config}")
     model = load_model_from_config(config, f"{opt.ckpt}")
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    if(torch.cuda.is_available()):
+        device = torch.device("cuda")
+    elif(torch.backends.mps.is_available()):
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
     model = model.to(device)
 
     clip_text_encoder = FrozenCLIPTextEmbedder(opt.clip_type).to(device)
--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -239,7 +239,12 @@ def main():
     config = OmegaConf.load(f"{opt.config}")
     model = load_model_from_config(config, f"{opt.ckpt}")
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    if(torch.cuda.is_available()):
+        device = torch.device("cuda")
+    elif(torch.backends.mps.is_available()):
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
     model = model.to(device)
 
     if opt.plms:
